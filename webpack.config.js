b1de71f156ab3bdf31090080308771c5bf1f80cb
CE Martin <16669684+candacemartin@users.noreply.github.com>
2022-07-12 19:37:10 -0400

    more updates

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const http = require('http');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client') + '/index.js',
+  output: {
+    path: path.resolve(__dirname, './bundle'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
+    ],
+  },
+};
