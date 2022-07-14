7c0affaf9af8ffd3b85650b0ed2c221c06b07d0e
CE Martin <16669684+candacemartin@users.noreply.github.com>
2022-07-14 17:57:48 -0400

    solo updates pre-presentation

--- client/components/App.js
+++ client/components/App.js
@@ -14,8 +14,12 @@ function FilterableShroomTable({ shrooms }) {
         <TechnicalChallengeThree />
       </div>
         <SearchBar />
-        <Button>Add Card</Button>
+        <Button />
       <div>
+      </div>
+      <div className='newCardBoxes'>
+        <NewCardOne />
+        <NewCardTwo />
       </div>
       <div className='existingRecordsBox'>
         <ShroomTable shrooms={shrooms} />
@@ -29,7 +33,7 @@ function Problem() {
   return (
     <div>
       <h1>what's the problem?</h1>
-      <p>medicinal mushrooms are fun to grow at home and have amazing health benefits<br />there are tons of apps for identifying mushrooms, but no good home cultivation guides/trackers </p>
+      <p className='prob1'>1. medicinal mushrooms are fun to grow at home and have amazing health benefits<br />2. there are tons of apps for identifying mushrooms, but no good home cultivation guides/trackers </p>
     </div>
   )
 }
@@ -38,18 +42,20 @@ function Solution() {
   return (
     <div>
       <h1>what's the solution?</h1>
-      <p>GROW ADVICE: kinokogrow.io will help you answer all the tough questions you encounter in your grow process (what substrate should i use? what are the ideal fruiting conditions?) <br />GROW TRACKING: successul mushroom cultivation is a truly iterative process! kinokogrow.io will help you keep track of all the details batch by batch.</p>
+      <p className='prob1'> *GROW ADVICE* kinokogrow.io will help you answer all the tough questions you encounter in your grow process (what substrate should i use? what are the ideal fruiting conditions?) 
+      <br />
+      <br />*GROW TRACKING* successul mushroom cultivation is a truly iterative process! kinokogrow.io will help you keep track of all the details batch by batch.</p>
     </div>
   )
 }
 
 function TechnicalChallengeOne() {
   return (
-    <button onClick={() => {
+    <button style={ {background: 'cadetblue'} } onClick={() => {
       console.log('WEBPACK CONFIG, of course.')
       return(
         <div>
-          <NewCard />
+          <NewCardOne />
         </div>
       )
     }}>tech challenge 1</button>
@@ -58,11 +64,11 @@ function TechnicalChallengeOne() {
 
 function TechnicalChallengeTwo() {
   return (
-    <button onClick={() => {
-      console.log('was very confused by the REACT part--it took me a long time to get these buttons on the screen and these console logs feel like a miracle tbh.')
+    <button style={{background: 'darkcyan'}} onClick={() => {
+      console.log('was very confused by the REACT part--it took me a long time to get these divs/buttons on the screen and these console logs feel like a miracle tbh.')
       return(
         <div>
-          <NewCard />
+          <NewCardOne />
         </div>
       )
     }}>tech challenge 2</button>
@@ -71,41 +77,80 @@ function TechnicalChallengeTwo() {
 
 function TechnicalChallengeThree() {
   return (
-    <button onClick={() => {
-      console.log('persisting DATA--i tried a postgres db and also tried to mess with redux/store, but got lost with both. what you see in green box is hard coded lol :)')
+    <button style={{background: 'lightslategray'}} onClick={() => {
+      console.log('persisting DATA--i tried a postgres db but could not get it to link up with buttons. i also tried to mess with redux/store, but got a bit turned around. data in green box is hardcoded lol :)')
       return(
         <div>
-          <NewCard />
+          <NewCardOne />
         </div>
       )
     }}>tech challenge 3</button>
   );
 }
 
-function NewCard() {
-  console.log('new card should render');
+function NewCardOne() {
+  console.log('new grow 1 rendered');
   return (
     <div className='newCardBox'>
-      this is just a box
+      <p>you started growing reishi
+        <br />on 7-14-2022
+        <br />in hardwood sawdust substrate
+      </p>
+      
     </div>
   )
 }
 
-function Button(){
+function NewCardTwo() {
+  console.log('new grow 2 rendered');
   return (
-    <button onClick={() => {
-      console.log('you clicked addCard')
-      return(
-        <div>
-          <NewCard />
-        </div>
-      )
-    }}>add card button</button>
-  );
+    <div className='newCardBox'>
+      <p>you started growing cordyceps militaris
+        <br />on 7-13-2022
+        <br />in brown rice substrate
+      </p>
+      
+    </div>
+  )
 }
 
 
+//search bar:
+function SearchBar() {
+  return (
+    <form>
+      <input
+        type='text'
+        //value={filterText}
+        placeholder='someday will work...'
+      />
+      <label>
+        <input
+          type='checkbox'
+          //checked={inStockOnly}
+        />{' '}
+        only show shrooms with images
+      </label>
+    </form>
+  );
+}
 
+//blue button for adding new grows:
+
+function Button(){
+  return (
+    <button className='firstButton' onClick={() => {
+      console.log('you clicked addCard')
+      return(
+        <div>
+          <NewCardOne />
+        </div>
+      )
+    }}>Grow Something New! (won't add anything yet tho)</button>
+  );
+}
+
+//static data box
 function ShroomCategoryRow({ category }) {
   return (
     <tr>
@@ -154,51 +199,32 @@ function ShroomTable({ shrooms }) {
   );
 }
 
-function SearchBar() {
-  return (
-    <form>
-      <input
-        type='text'
-        //value={filterText}
-        placeholder='Search...'
-      />
-      <label>
-        <input
-          type='checkbox'
-          //checked={inStockOnly}
-        />{' '}
-        only show shrooms with images
-      </label>
-    </form>
-  );
-}
-
 const SHROOMS = [
   {
     category: '',
-    benefits: ['test', 'test'],
-    image: [],
-    name: 'maitake',
-    psychedelic: false
-  },
-  {
-    category: '',
-    benefits: ['test', 'test'],
-    image: [],
-    name: "lion's mane",
-    psychedelic: false
-  },
-  {
-    category: '',
-    benefits: ['test', 'test'],
+    benefits: ['mush ', 'rooms'],
     image: [],
     name: 'cordyceps',
     psychedelic: false
   },
   {
     category: '',
-    benefits: ['test', 'test'],
-    image: ['client/images/golden_teachers.jpeg'],
+    benefits: ['cured', '  me', ' test'],
+    image: [],
+    name: "lion's mane",
+    psychedelic: false
+  },
+  {
+    category: '',
+    benefits: ['jk not evaluated by FDA'],
+    image: [],
+    name: 'maitake',
+    psychedelic: false
+  },
+  {
+    category: '',
+    benefits: ['test', '' ],
+    image: ['test', 'test'],
     name: 'reishi',
     psychedelic: false
   },
@@ -211,7 +237,7 @@ const SHROOMS = [
   },
   {
     category: '',
-    benefits: ['test', 'test'],
+    benefits: ['happy'],
     image: [],
     name: 'golden teacher',
     psychedelic: false
